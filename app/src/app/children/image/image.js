8ddcdee8557e17b60c61fb5df8282804dbd1ff4a
Laura Kenny <l.j.kenny@hotmail.com>
2023-11-14 15:09:49 +0000

    provide image sourceset with webp format.

--- app/src/app/children/image/image.js
+++ app/src/app/children/image/image.js
@@ -1,11 +1,13 @@
 import styles from './image.module.css'
 
 export const Image = ({ imageData: { imageText, imageUrl } }) => {
+
   return (
-    <img
-      src={imageUrl}
-      className={styles.image}
-      alt={imageText}
-    />
+  
+  <picture className={styles.image}>
+    <source media="(max-width: 400px)" srcset="image-400.webp" />
+    <source media="(min-width: 600px)" srcset="image.webp" />
+    <img src="image.png" alt={imageText} />
+</picture>
   )
 }
