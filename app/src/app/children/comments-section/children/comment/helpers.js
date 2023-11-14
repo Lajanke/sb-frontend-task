e981a98a965868a9a378c67d5a29979a92a72791
Laura Kenny <l.j.kenny@hotmail.com>
2023-11-14 15:11:59 +0000

    linting

--- app/src/app/children/comments-section/children/comment/helpers.js
+++ app/src/app/children/comments-section/children/comment/helpers.js
@@ -8,7 +8,7 @@ export const formatComment = (comment, userName) => {
   let formattedComment = comment
 
   matches.forEach((match) => {
-    //included a blank href so hashtags still act like links as there is no link information available in the given data. 
+    // included a blank href so hashtags still act like links as there is no link information available in the given data.
     formattedComment = formattedComment.replace(match, `<a href=''>${match}</a>`)
   })
 
