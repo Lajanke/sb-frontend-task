9eacd5a238789cfec90c64758f219adc7f992f89
Laura Kenny <l.j.kenny@hotmail.com>
2023-11-14 03:31:53 +0000

    format data in separate function

--- app/src/app/index.js
+++ app/src/app/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { formatData } from './helpers'
 import { Image } from './children/image'
 import { LikesSection } from './children/likes-section'
 import { UserSection } from './children/user-section'
@@ -6,41 +7,7 @@ import { CommentsSection } from './children/comments-section'
 import styles from './styles.module.css'
 
 const App = (props) => {
-  const imageText = props.data.edge_media_to_caption.edges[0].node.text
-  const imageUrl = props.data.display_url
-
-  const userLocation = props.data.location.name
-  const userName = props.data.owner.username
-  const userImage = props.data.owner.profile_pic_url
-
-  const likedByViewer = props.data.viewer_has_liked
-  const numberOfLikes = props.data.edge_media_preview_like.count
-  const datePosted = props.data.taken_at_timestamp
-
-  const comments = props.data.edge_media_to_comment.edges
-
-  const imageData = {
-    imageText,
-    imageUrl
-  }
-
-  const userData = {
-    userImage,
-    userName,
-    userLocation
-  }
-
-  const likesData = {
-    likedByViewer,
-    numberOfLikes,
-    datePosted
-  }
-
-  const commentData = {
-    comments,
-    captionComment: imageText,
-    captionUserName: userName
-  }
+  const {imageData, userData, commentData, likesData} = formatData(props.data)
 
   return (
     <main className={styles.main}>
